19b7f4f3e40e985e0b936fbb4a4dfce2376c1c98
macnick <nick@macexperts.gr>
2019-11-26 12:13:28 +0200

    Create flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,24 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
     def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = 'Your account was created successfully'
+            redirect_to @user
+        else
+            flash[:danger] = 'User was not created'
+            render 'new'
+        end
     end
+
     def show
     end
+
+    private
+
+        def user_params
+            params.require(:user).permit(:name, :email)
+        end
 end
