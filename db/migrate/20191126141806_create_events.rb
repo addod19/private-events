4c4c55f924309d3a6f9d6625f5aee8f2c0048548
macnick <nick@macexperts.gr>
2019-11-27 18:45:28 +0200

    Fix bug

--- db/migrate/20191126141806_create_events.rb
+++ db/migrate/20191126141806_create_events.rb
@@ -4,7 +4,6 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.string :title
       t.string :location
       t.integer :user_id
-      t.text :description
       t.datetime :date
 
       t.timestamps
--- db/migrate/20191127150809_add_description_to_event.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToEvent < ActiveRecord::Migration[5.2]
-  def change
-    add_column :events, :description, :text
-  end
-end
