bcc19a25c4e77c3a51f07d2b8614856beb3ba6d5
addod19 <addodaniellarbi@gmail.com>
2019-11-27 14:54:39 +0000

    create event controller,template to create show and list events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,32 @@
 class EventsController < ApplicationController
 
+    def index
+        @events = Event.all
+    end
+
+    def new
+        @event = current_user.events.build
+    end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+            flash[:success] = 'Your account was created successfully'
+            redirect_to @event
+        else
+            flash[:danger] = 'User was not created'
+            render 'new'
+        end
+    end
+
+    def show
+        @event = Event.find(params[:id]) 
+    end
+
+    private
+
+        def event_params
+            params.require(:event).permit(:title, :location, :date)
+        end
   
 end
