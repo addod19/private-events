3e41064f843413a9d5ad98e25d3285f180df5865
macnick <nick@macexperts.gr>
2019-12-02 18:46:46 +0200

    Fix 2 rubocop errors

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -14,30 +14,26 @@ RSpec.describe Event, type: :model do
 
   context 'event valid validations test' do
     it 'is invalid without event title' do
-      event = Event.new(title: '', location: 'Accra', user_id: '2',
-                        description: "Daniel's party with Jack Daniels",
+      event = Event.new(title: '', location: 'Accra', user_id: '2', description: "Daniel's party with Jack Daniels",
                         date: '2020-02-03')
       event.valid?
       expect(event.errors[:title]).to include("can't be blank")
     end
     it 'is invalid without event location' do
       event = Event.new(title: 'Birthday party', location: '', user_id: '2',
-                        description: "Daniel's party with Jack Daniels",
-                        date: '2020-02-03')
+                        description: "Daniel's party with Jack Daniels", date: '2020-02-03')
       event.valid?
       expect(event.errors[:location]).to include("can't be blank")
     end
     it 'is invalid without event description' do
       event = Event.new(title: 'Birthday party', location: '', user_id: '2',
-                        description: '',
-                        date: '2020-02-03')
+                        description: '', date: '2020-02-03')
       event.valid?
       expect(event.errors[:description]).to include("can't be blank")
     end
     it 'is invalid without event date' do
       event = Event.new(title: 'Birthday party', location: '', user_id: '2',
-                        description: '',
-                        date: '')
+                        description: '', date: '')
       event.valid?
       expect(event.errors[:date]).to include("can't be blank")
     end
@@ -46,29 +42,25 @@ RSpec.describe Event, type: :model do
   context 'event invalid validations test' do
     it 'is invalid with description longer than specified' do
       event = Event.new(title: 'party' * 10, location: '', user_id: '2',
-                        description: 'abdcefghjklmpnop' * 20,
-                        date: '2020-02-03')
+                        description: 'abdcefghjklmpnop' * 20, date: '2020-02-03')
       event.valid?
       expect(event.errors[:description]).to include('is too long (maximum is 250 characters)')
     end
     it 'is invalid with description shorter than specified' do
       event = Event.new(title: 'party' * 10, location: '', user_id: '2',
-                        description: 'abdc',
-                        date: '2020-02-03')
+                        description: 'abdc', date: '2020-02-03')
       event.valid?
       expect(event.errors[:description]).to include('is too short (minimum is 30 characters)')
     end
     it 'is invalid with title shorter than specified' do
       event = Event.new(title: 'party', location: '', user_id: '2',
-                        description: '',
-                        date: '2020-02-03')
+                        description: '', date: '2020-02-03')
       event.valid?
       expect(event.errors[:title]).to include('is too short (minimum is 10 characters)')
     end
     it 'is invalid with title longer than specified' do
       event = Event.new(title: 'party' * 10, location: '', user_id: '2',
-                        description: '',
-                        date: '2020-02-03')
+                        description: '', date: '2020-02-03')
       event.valid?
       expect(event.errors[:title]).to include('is too long (maximum is 40 characters)')
     end
