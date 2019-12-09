77af07b2424b10630764ac29e4de295e543024da
addod19 <addodaniellarbi@gmail.com>
2019-12-09 19:31:54 +0000

    resolve lint error while merging

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many :invitations, foreign_key: 'invitee'
   has_many :invited_events, through: :invitations
 
-
   validates :name, presence: true, length: { minimum: 10, maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 150 },
